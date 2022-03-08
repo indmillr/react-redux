a8370ff825217e016849d64d8ef9fd8b5035d251
Ian Miller <theiandmiller@gmail.com>
2022-03-08 05:43:35 -0600

    JSX to JS references

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,20 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function getButtonText() {
-  return "Click On Me!";
-}
-
 // Create a React Component
 const App = () => {
+  const buttonText = { text: "Click Me" };
+  const labelText = "Enter Name:";
+
   return (
     <div>
-      <label className='label' for='name'>
-        Enter Name:
+      <label className='label' htmlFor='name'>
+        {labelText}
       </label>
       <input id='name' type='text' />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {getButtonText()}
+        {buttonText.text}
       </button>
     </div>
   );
