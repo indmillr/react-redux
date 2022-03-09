badefda1ec0c383810dbb40153e5a79dea60f09c
Ian Miller <theiandmiller@gmail.com>
2022-03-09 06:32:11 -0600

    constructor prop and super props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  constructor(props) {
+    // A reference to the parent constructor function
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  // React REQUIRES a render method!!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
