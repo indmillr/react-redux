6aa075bd3d71255227946ed397fd91b9de270fc4
Ian Miller <theiandmiller@gmail.com>
2022-03-10 04:40:43 -0600

    logic to getSeason

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,43 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   // Good place to do one-time setup
-    this.state = { lat: null, errorMessage: "" }
-  }
-
-  state = { lat: null, errorMessage: '' };
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
+      (err) => this.setState({ errorMessage: err.message }),
     );
   }
 
   render() {
     // Conditional rendering to handle Errors
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
+      return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div> 
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading!</div>;
   }
-
-  componentDidMount() {
-    console.log("Component loaded!");
-  }
-
-  componentDidUpdate() {
-    console.log("Component rerendered");
-  }
-  
-  componentWillUnmount() {
-  }
-
 }
 
-ReactDOM.render(<App />, document.querySelector(#root));
+ReactDOM.render(<App />, document.querySelector("#root"));
