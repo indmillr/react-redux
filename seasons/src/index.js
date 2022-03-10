df385d7c5121419252ae96cd5b7cb860d4450b9e
Ian Miller <theiandmiller@gmail.com>
2022-03-10 04:24:29 -0600

    refactor to remove component/super

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,27 +4,18 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   // Good place to do one-time setup
-  constructor(props) {
-    // A reference to the parent constructor function
-    super(props);
+    this.state = { lat: null, errorMessage: "" }
+  }
 
-    // THIS IS THE ONLY TIME to directly assign this.state
-    this.state = { lat: null, errorMessage: "" };
+  state = { lat: null, errorMessage: '' };
 
-    // Do not put a call like this within the render method
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // to update state HAVE to use setState
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  // React REQUIRES a render method!!!
-  // Avoid anything besides returning JSX
   render() {
     // Conditional rendering to handle Errors
     if (this.state.errorMessage && !this.state.lat) {
@@ -35,21 +26,15 @@ class App extends React.Component {
     }
     return <div>Loading!</div>;
   }
-  // Content visible on the screen
 
-  // Good place to do data-loading
   componentDidMount() {
     console.log("Component loaded!");
   }
-  // Sit and Wait for updates
 
-  // Good place to do more data-loading when state/props change
   componentDidUpdate() {
     console.log("Component rerendered");
   }
-  // Sit and wait until this component is no longer shown
   
-  // Good place to do cleanup (especially for non-React stuff)
   componentWillUnmount() {
   }
 
