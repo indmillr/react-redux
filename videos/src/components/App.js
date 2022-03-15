47db38afe09ea1ba1f0c0fac7026641524f46781
Ian Miller <theiandmiller@gmail.com>
2022-03-15 05:29:26 -0500

    wire App.js to VideoList.js

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [] };
+
   // What to do when the form is submitted?
   // Send the API request (async)
   onTermSubmit = async (term) => {
@@ -11,12 +14,16 @@ class App extends React.Component {
         q: term,
       },
     });
+
+    // set State with the list of videos returned as 'response'
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
