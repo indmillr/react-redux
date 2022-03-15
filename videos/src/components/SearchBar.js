175e9c92c3a71b69a28e35dc9f2e45bb65210954
Ian Miller <theiandmiller@gmail.com>
2022-03-15 04:52:06 -0500

    event handlers for input entry and submit

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -3,13 +3,33 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  // what happens when Input changes?
+  onInputChange = (event) => {
+    // updates state (term) with target (input) value
+    this.setState({ term: event.target.value });
+  };
+
+  // what happens when Form is submitted?
+  onFormSubmit = (event) => {
+    // stop refresh from happening after each char
+    event.preventDefault();
+
+    // TODO: Make sure to call callback from Parent Component
+  };
+
   render() {
     return (
       <div className='ui segment search-bar'>
-        <form className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Video Search</label>
-            <input type='text' value={this.state.term} />
+            <input
+              type='text'
+              // updates value with state
+              value={this.state.term}
+              // invokes onInputChange when changes occur
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
