f102221cc268758b3fcdb923fa686ea1fb1440b4
Ian Miller <theiandmiller@gmail.com>
2022-03-15 05:09:59 -0500

    setup YouTube API

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDhqj-xzcSPvUBy3qcJM1FIwt7dANuKRiY";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
