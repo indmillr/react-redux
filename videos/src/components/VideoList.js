55a659e1b22000e374feb1d80bd8c173f9ef1ac5
Ian Miller <theiandmiller@gmail.com>
2022-03-15 06:35:54 -0500

    fix errors

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -5,7 +5,13 @@ import VideoItem from "./VideoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
   // return a new array
   const renderedList = videos.map((video) => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
 
   return <div className='ui relaxed divided list'>{renderedList}</div>;
