50bef47929b8d8c11f726aebb120e2c213ea62f9
Ian Miller <theiandmiller@gmail.com>
2022-03-15 05:34:05 -0500

    VideoItem component setup

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,14 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+// destructure from props.videos
+const VideoList = ({ videos }) => {
+  // return a new array
+  const renderedList = videos.map((video) => {
+    return <VideoItem />;
+  });
+
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
