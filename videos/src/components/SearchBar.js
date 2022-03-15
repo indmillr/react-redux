0a13b2064bb335a683f536496e0be52d4e1e71f1
Ian Miller <theiandmiller@gmail.com>
2022-03-15 04:41:40 -0500

    SearchBar styling

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className='ui segment search-bar'>
+        <form className='ui form'>
+          <div className='field'>
+            <label>Video Search</label>
+            <input type='text' value={this.state.term} />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
