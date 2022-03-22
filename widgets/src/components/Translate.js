a513a462240fcc5b6356144f4b0ebb85339cc4e3
Ian Miller <theiandmiller@gmail.com>
2022-03-22 18:51:07 -0500

    remove hardcoded Label and replace with 'label' prop

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -17,9 +17,16 @@ const options = [
 ];
 
 const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+
   return (
     <div>
-      <Dropdown />
+      <Dropdown
+        label='Select  a Language'
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
     </div>
   );
 };
